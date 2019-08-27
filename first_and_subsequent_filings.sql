c69dba468b39ee5d4e51502eba2adf7d62928003
Florian Seliger <seliger@kof.ethz.ch>
2019-08-27 17:13:56 +0200

    Update first_and_subsequent_filings.sql

--- first_and_subsequent_filings.sql
+++ first_and_subsequent_filings.sql
@@ -7,26 +7,29 @@
 -- Finally, we insert singletons (there are no subsequent filings for singletons). 
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+
 DROP TABLE IF EXISTS first_and_subsequent_filings;
 CREATE TABLE first_and_subsequent_filings AS
-SELECT t1.prior_appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind
-FROM
-tls204_appln_prior t1
+SELECT DISTINCT t1.prior_appln_id, t2.*, t3.appln_auth, t3.appln_kind, t3.appln_filing_date, t3.appln_filing_year, t3.earliest_publn_date, t3.earliest_publn_year, t3.earliest_pat_publn_id, t3.granted, t3.docdb_family_id, t3.inpadoc_family_id  FROM
+tls204_appln_prior t1 
 JOIN tls211_pat_publn t2 
 ON t1.appln_id = t2.appln_id
+JOIN tls201_appln t3
+ON t1.appln_id = t3.appln_id
 UNION
-SELECT t1.prior_appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind
-FROM
+SELECT DISTINCT t1.prior_appln_id, t2.*, t3.appln_auth, t3.appln_kind, t3.appln_filing_date, t3.appln_filing_year, t3.earliest_publn_date, t3.earliest_publn_year, t3.earliest_pat_publn_id, t3.granted, t3.docdb_family_id, t3.inpadoc_family_id  FROM
 tls204_appln_prior t1
 JOIN tls211_pat_publn t2
 ON t1.prior_appln_id = t2.appln_id
+JOIN tls201_appln t3
+ON t1.prior_appln_id = t3.appln_id
 ;
 
 ALTER TABLE first_and_subsequent_filings ADD COLUMN TYPE TEXT;
 UPDATE first_and_subsequent_filings set TYPE = 'PRIORITY';
 
 INSERT INTO first_and_subsequent_filings
-SELECT t1.appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind, 'PCT'  
+SELECT DISTINCT t1.appln_id, t2.*, t1.appln_auth, t1.appln_kind, t1.appln_filing_date, t1.appln_filing_year, t1.earliest_publn_date, t1.earliest_publn_year, t1.earliest_pat_publn_id, t1.granted, t1.docdb_family_id, t1.inpadoc_family_id, 'PCT'  
  FROM tls201_appln t1
 JOIN tls211_pat_publn t2 
 ON t1.appln_id = t2.appln_id
@@ -34,7 +37,7 @@ LEFT OUTER JOIN first_and_subsequent_filings t3 on t1.appln_id = t3.appln_id
 WHERE internat_appln_id = 0 and t1.appln_kind = 'W' and t3.appln_id is null and t1.appln_id = t1.earliest_filing_id;
 
 INSERT INTO first_and_subsequent_filings
-SELECT t1.internat_appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind, 'PCT'  
+SELECT DISTINCT t1.internat_appln_id, t2.*, t1.appln_auth, t1.appln_kind, t1.appln_filing_date, t1.appln_filing_year, t1.earliest_publn_date, t1.earliest_publn_year, t1.earliest_pat_publn_id, t1.granted, t1.docdb_family_id, t1.inpadoc_family_id, 'PCT'  
  FROM tls201_appln t1
 JOIN tls211_pat_publn t2 
 ON t1.appln_id = t2.appln_id
@@ -44,40 +47,48 @@ and t3.TYPE = 'PCT';
 
 
 INSERT INTO first_and_subsequent_filings
-SELECT t1.parent_appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind, 'CONTINUAL'  
+SELECT DISTINCT t1.parent_appln_id, t2.*, t3.appln_auth, t3.appln_kind, t3.appln_filing_date, t3.appln_filing_year, t3.earliest_publn_date, t3.earliest_publn_year, t3.earliest_pat_publn_id, t3.granted, t3.docdb_family_id, t3.inpadoc_family_id, 'CONTINUAL'  
  FROM TLS216_APPLN_CONTN t1
 JOIN tls211_pat_publn t2 
 ON t1.parent_appln_id = t2.appln_id
-LEFT OUTER JOIN first_and_subsequent_filings t3 on t1.parent_appln_id = t3.appln_id
-WHERE t3.appln_id is null;
+JOIN tls201_appln t3
+ON t1.parent_appln_id = t3.appln_id
+LEFT OUTER JOIN first_and_subsequent_filings t4 on t1.parent_appln_id = t4.appln_id
+WHERE t4.appln_id is null;
 
 INSERT INTO first_and_subsequent_filings
-SELECT t1.parent_appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind, 'CONTINUAL'  
+SELECT DISTINCT t1.parent_appln_id, t2.*, t3.appln_auth, t3.appln_kind, t3.appln_filing_date, t3.appln_filing_year, t3.earliest_publn_date, t3.earliest_publn_year, t3.earliest_pat_publn_id, t3.granted, t3.docdb_family_id, t3.inpadoc_family_id, 'CONTINUAL'  
  FROM TLS216_APPLN_CONTN t1
 JOIN tls211_pat_publn t2 
 ON t1.appln_id = t2.appln_id
-JOIN first_and_subsequent_filings t3 on t1.parent_appln_id = t3.prior_appln_id;
+JOIN tls201_appln t3
+ON t1.appln_id = t3.appln_id
+JOIN first_and_subsequent_filings t4 on t1.parent_appln_id = t4.prior_appln_id;
 
 INSERT INTO first_and_subsequent_filings
-SELECT t1.tech_rel_appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind, 'TECH_REL'  
+SELECT DISTINCT t1.tech_rel_appln_id, t2.*, t3.appln_auth, t3.appln_kind, t3.appln_filing_date, t3.appln_filing_year, t3.earliest_publn_date, t3.earliest_publn_year, t3.earliest_pat_publn_id, t3.granted, t3.docdb_family_id, t3.inpadoc_family_id, 'TECH_REL'  
  FROM TLS205_TECH_REL t1
 JOIN tls211_pat_publn t2 
 ON t1.tech_rel_appln_id = t2.appln_id
-LEFT OUTER JOIN first_and_subsequent_filings t3 on t1.tech_rel_appln_id = t3.appln_id
-WHERE t3.appln_id is null;
+JOIN tls201_appln t3
+ON t1.tech_rel_appln_id = t3.appln_id
+LEFT OUTER JOIN first_and_subsequent_filings t4 on t1.tech_rel_appln_id = t4.appln_id
+WHERE t4.appln_id is null;
 
 INSERT INTO first_and_subsequent_filings
-SELECT t1.tech_rel_appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind, 'TECH_REL'  
- FROM TLS205_TECH_REL t1
+SELECT DISTINCT t1.tech_rel_appln_id, t2.*, t3.appln_auth, t3.appln_kind, t3.appln_filing_date, t3.appln_filing_year, t3.earliest_publn_date, t3.earliest_publn_year, t3.earliest_pat_publn_id, t3.granted, t3.docdb_family_id, t3.inpadoc_family_id, 'TECH_REL'  
+FROM TLS205_TECH_REL t1
 JOIN tls211_pat_publn t2 
 ON t1.appln_id = t2.appln_id
-JOIN first_and_subsequent_filings t3 on t1.tech_rel_appln_id = t3.prior_appln_id;
+JOIN tls201_appln t3
+ON t1.appln_id = t3.appln_id
+JOIN first_and_subsequent_filings t4 on t1.tech_rel_appln_id = t4.prior_appln_id;
 
 
 -- Singletons
 
 INSERT INTO first_and_subsequent_filings
-SELECT t1.appln_id, t2.appln_id, t2.publn_auth, t2.publn_nr, t2.publn_nr_original, t2.publn_kind, 'SINGLE' 
+SELECT DISTINCT t1.appln_id, t2.*, t1.appln_auth, t1.appln_kind, t1.appln_filing_date, t1.appln_filing_year, t1.earliest_publn_date, t1.earliest_publn_year, t1.earliest_pat_publn_id, t1.granted, t1.docdb_family_id, t1.inpadoc_family_id, 'SINGLE' 
   FROM tls201_appln t1
 JOIN tls211_pat_publn t2 
 ON t1.appln_id = t2.appln_id
